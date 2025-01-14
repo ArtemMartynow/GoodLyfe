1c45cc86ce129e95e8685f9590568495efaec134
ArtemMartynow <artem1435@i.ua>
2025-01-14 16:57:29 +0100

    made a responsive real stories

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,18 @@
-  const swiper = new Swiper('.swiper', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    autoplay: {
-      delay: 5000,
-    },
-  })
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+  },
+})
+
+const swiperSecond = new Swiper('.swiper-second', {
+  slidesPerView: 1.2,
+  spaceBetween: 26,
+  loop: false,
+  slidesPerGroup: 1,
+  centeredSlides: true,
+})
